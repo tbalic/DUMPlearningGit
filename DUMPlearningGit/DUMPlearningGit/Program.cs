5faf932896f27dec4f579ef45fedfa3626f629d8
tbalic <balic.tihana@gmail.com>
2020-11-18 23:57:52 +0100

    Added second commit

--- DUMPlearningGit/DUMPlearningGit/Program.cs
+++ DUMPlearningGit/DUMPlearningGit/Program.cs
@@ -7,6 +7,7 @@ namespace DUMPlearningGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Line added for second commit.");
         }
     }
 }
